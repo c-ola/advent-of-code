51c3dc425f1b5d13a3662a2ebb6a85f8e77e960d
Nikola Zupancic <nikola.z37@hotmail.com>
2024-12-01 13:30:25 -0500

    updated

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,33 +1,18 @@
 use std::fs::read_to_string;
 
-pub trait SplitOdd<T> {
-    fn split_odd(self) -> (Vec<T>, Vec<T>);
-}
-
-impl<T: Sized, I: IntoIterator<Item = T>> SplitOdd<T> for I {
-    fn split_odd(self) -> (Vec<T>, Vec<T>) {
-        let mut a: Vec<T> = Vec::new();
-        let mut b: Vec<T> = Vec::new();
-        let mut i = 0;
-        for item in self {
-            if i % 2 == 0 {
-                a.push(item);
-            } else {
-                b.push(item);
-            }
-            i += 1
-        }
-        (a, b)
-    }
-}
-
 fn part1() {
     let data = read_to_string("inputs/day1.txt").unwrap();
-    let data = data.split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
-    let (mut left, mut right) = data.into_iter().split_odd();
-    //println!("{left:?}, {right:?}");
-    left.sort();
+    let data = data
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let (left, right): (Vec<_>, Vec<_>) =
+        data.into_iter().enumerate().partition(|(i, _)| i % 2 == 0);
+    let mut right: Vec<usize> = right.into_iter().map(|(_, x)| x).collect();
     right.sort();
+    let mut left: Vec<usize> = left.into_iter().map(|(_, x)| x).collect();
+    left.sort();
+
     let mut sum = 0;
     for i in 0..left.len() {
         sum += left[i].abs_diff(right[i]);
@@ -37,12 +22,23 @@ fn part1() {
 
 fn part2() {
     let data = read_to_string("inputs/day1.txt").unwrap();
-    let data = data.split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
-    let (left, right) = data.into_iter().split_odd();
+    let data = data
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let (left, right): (Vec<_>, Vec<_>) =
+        data.into_iter().enumerate().partition(|(i, _)| i % 2 == 0);
+    let mut right: Vec<usize> = right.into_iter().map(|(_, x)| x).collect();
+    right.sort();
+    let left: Vec<usize> = left.into_iter().map(|(_, x)| x).collect();
 
     let mut sum = 0;
     for i in 0..left.len() {
-        let count = right.iter().filter(|x| **x == left[i]).collect::<Vec<_>>().len();
+        let count = right
+            .iter()
+            .filter(|x| **x == left[i])
+            .collect::<Vec<_>>()
+            .len();
         sum += left[i] * count;
     }
     println!("{sum}");
